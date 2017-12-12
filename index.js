40aee3fed88bcfc2cf7e1ebd6a6cdf64daae8017
John Tuttle <thejwt@gmail.com>
2017-12-12 14:13:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,17 @@ deliverFood.apply(pbj, ["Jesse", "15"]);
 
 serve.call(gc);
 serve.apply(pbj, ["Terry", "Tom", "Tabitha"]);
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+var sally = new Customer("Sally", "4");
+ 
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
