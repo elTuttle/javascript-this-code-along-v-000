a601b8940fcf2c428a09805a41fb7051edbc9db7
John Tuttle <thejwt@gmail.com>
2017-12-12 13:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function Sandwich(bread, ingredients, cut) {
   this.ingredients = ingredients;
   this.cut = cut;
 }
- 
+
 var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
 var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
+
+prj.serve();
